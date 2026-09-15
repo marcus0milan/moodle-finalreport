b513228bc26192b3487b6469e06f405bf10330c9
Sepolino <viniszsx505@gmail.com>
2026-09-14 23:30:30 -0300

    Update plugin details for final report version

--- version.php
+++ version.php
@@ -1,8 +1,9 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->component = 'local_seuplugin'; // frankenstyle: tipo_nome
-$plugin->version   = 2024091400;        // AAAAMMDDXX
-$plugin->requires  = 2022041900;        // versão mínima do Moodle
-$plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0.0';
+$plugin->component = 'local_finalreport';
+$plugin->version = 2026091400;
+$plugin->requires = 2024100700;
+$plugin->supported = [405, 405];
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0.0';
