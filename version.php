27947936ce256804fba47c5729e524e4b1ad2125
Sepolino <viniszsx505@gmail.com>
2026-09-14 23:28:39 -0300

    first commit

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_seuplugin'; // frankenstyle: tipo_nome
+$plugin->version   = 2024091400;        // AAAAMMDDXX
+$plugin->requires  = 2022041900;        // versão mínima do Moodle
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '1.0.0';
