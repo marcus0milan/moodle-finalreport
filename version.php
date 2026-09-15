b448b4bc5652cd4fd30e8e161b2726a86e671549
Sepolino <viniszsx505@gmail.com>
2026-09-14 23:31:11 -0300

    Add license information to version.php

--- version.php
+++ version.php
@@ -1,4 +1,11 @@
 <?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_finalreport';
