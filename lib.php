adc72a889febce994a68124f76978356e9cb92ad
Sepolino <viniszsx505@gmail.com>
2026-09-14 23:49:37 -0300

    Add report_finalreport_extend_navigation_course function to lib.php

--- lib.php
+++ lib.php
@@ -14,3 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle. If not, see <https://www.gnu.org/licenses/>.
 
+function report_finalreport_extend_navigation_course($navigation, $course, $context) {
+    
+}
